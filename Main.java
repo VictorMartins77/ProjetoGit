14d1d53ce9d541761f1d0e420e93f5780826aa26
William <psilva546@hotmail.com>
2024-04-20 10:35:51 -0300

    Adicionei whos

--- Main.java
+++ Main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Whos whos = new Whos();
+
         String[] meses = {
             "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
             "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
@@ -13,6 +16,7 @@ public class Main {
 
         if (numeroDoMes >= 1 && numeroDoMes <= 12) {
             System.out.println("O mês correspondente é: " + meses[numeroDoMes - 1]);
+            System.out.println(whos.printaNome());//printa who is
         } else {
             System.out.println("Número inválido. Por favor, insira um número entre 1 e 12.");
         }
